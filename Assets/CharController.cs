14a3d335c2c597e022f56529f900ba2af47e5a0d
Daijaan DavisLeahy <DDavisLeahy2018@elam.co.uk>
2018-10-01 16:37:37 +0100

    faceless unity project (creative :) )

---
+++ Assets/CharController.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+    Rigidbody2D myChar;
+
+    Animator charAnim;
+
+    public Transform groundCheck;
+
+    public LayerMask ground;
+
+    float groundRadius = 0.2f;
+
+    bool isGrounded = false;
+    public float maxSpeed = 10.0f;
+
+    public float jumpForce = 700.0f;
+
+        //Private variables to track whether or not character is facing left or right or if our double jump has been used.
+    bool facingRight = true;
+    bool facingLeft = false;
+    bool doubleJump = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
